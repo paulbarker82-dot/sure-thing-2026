0ad862a28c1378d3fcf7fb15b4691ca68db15a1c
paulbarker82-dot <paulbarker82@gmail.com>
2026-04-25 10:41:30 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = 'sure-thing-v1';
-const ASSETS = ['/', '/public.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
